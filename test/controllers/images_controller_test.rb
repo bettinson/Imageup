98026008f82be4fcf7c097a422328853f487dcb9
Matt Bettinson <mattbettinson@me.com>
2016-10-19 12:02:31 -0400

    Added tests for image upload

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -3,7 +3,13 @@ require 'test_helper'
 class ImagesControllerTest < ActionDispatch::IntegrationTest
 
   setup do
+    # Image is the model, picture is the actual file
     @image = images(:one)
+    @picture = fixture_file_upload('test/fixtures/files/test_image.png','application/png')
+  end
+
+  def file_data(name)
+    File.read(Rails.root.to_s + "/test/fixtures/files/#{name}")
   end
 
   test "should get display" do
@@ -16,14 +22,14 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get upload" do
-    get images_upload_url
+  test "should get create pages" do
+    get images_create_url
     assert_response :success
   end
 
   test "should upload image" do
     assert_difference('Image.count') do
-      post images_create_url, params: { image: { title: 'Hey'} }
+      post images_upload_url, params: { image: { title: 'Hey', picture: @picture } }
     end
 
     assert_redirected_to images_index_url
