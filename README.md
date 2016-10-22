d69eb74f4efc9ce479b0de5d23d9d6787b39cbb6
Matt Bettinson <mattbettinson@me.com>
2016-10-22 11:28:02 -0400

    Made basic user model with tests

--- README.md
+++ README.md
@@ -1,14 +1,22 @@
 # Image uploader
 
-Get started with:
+## About
 
-`bundle install --without production && rake db:migrate`
+This is my minimal image uploader. I eventually want to expand this into a Tumblr-like service, with tags, user pages, searching, following, etc.
+
+## Get started
+
+`$ bundle install --without production && rake db:migrate`
 
 Run tests with:
 
-`rake`
+`$ rake`
 
-Eventual roadmap:
+Load server with:
+
+`$ rails server` and then go to `http://localhost:3000`
+
+## Eventual roadmap:
 
 - Index page with thumbnails created by worker tasks
 - Users
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161017200635) do
+ActiveRecord::Schema.define(version: 20161022151715) do
 
   create_table "images", force: :cascade do |t|
     t.string   "path"
@@ -21,8 +21,11 @@ ActiveRecord::Schema.define(version: 20161017200635) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
