6f7a0396ca97b62e05e776536fe6d4008dc9a381
Matt Bettinson <mattbettinson@me.com>
2016-10-25 13:12:12 -0400

    Changed db file to use absolute path for production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,6 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-ENV['DATABASE_URL'] = "/home/matt/db/production.sqlite3";
 
 
 # role-based syntax
