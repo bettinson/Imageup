0140a2c9ef2b18ded774e7f226af58b3fbc37032
Matt Bettinson <mattbettinson@me.com>
2016-10-25 15:12:35 -0400

    Add logging in/out with tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log in user
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = "Invalid email/password combination"
       render 'new'
@@ -13,5 +14,9 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    respond_to do |format|
+      format.html { redirect_to root_url, notice: "Logged out." }
+    end
   end
 end
