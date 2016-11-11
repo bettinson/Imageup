b4a0629d4db7b9b47a311daacc01b81f8f601ff9
Matt Bettinson <mattbettinson@me.com>
2016-11-11 09:34:29 -0500

    Added following and unfollowing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,10 +39,5 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
-  def logged_in_user
-    unless logged_in?
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
+  
 end
